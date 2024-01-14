460bb65f3cca00a934c0d8d21f84077d98605501
azamsajjad <azamsaws@gmail.com>
2024-01-15 01:04:06 +0500

    pipeline

--- 2-crud-pipeline-setup/infra/remote_backend_s3.tf
+++ 2-crud-pipeline-setup/infra/remote_backend_s3.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        bucket = "devops-project-1-app-remote-state-bucket"
+        bucket = "devops-project-1-app-remote-state-bucket-west"
         key = "devops-project-1/terraform.tfstate"
         region = "us-west-1"
     }
